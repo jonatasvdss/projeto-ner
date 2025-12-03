556e1cc547aed9e3c5d9e8f5f146391230379b17
Jônatas Vicente da Silva <jonatasvdss@gmail.com>
2025-12-03 16:07:46 +0000

    Melhora a visualização da saída do teste.py e ajusta estilização

--- teste.py
+++ teste.py
@@ -25,7 +25,17 @@ if __name__ == "__main__":
         meta_id_reu
       )
 
-      print(resultado)
+      print(f"Sucesso geral? {resultado['sucesso_geral']}")
+      
+      print("\n", "----------------------- IDs ----------------------", end="")
+      print(" ------ Status ------")
+      for id in resultado["comparacao_ids"]:
+         print(f"{id['alvo']:<50} | {id['status']}")
+      
+      print("\n", "---------------------- Nomes ---------------------", end="")
+      print(" ------ Status ------")
+      for nome in resultado["comparacao_nomes"]:
+         print(f"{nome['alvo']:<50} | {nome['status']}")
 
     except IndexError:
        print("Índice deve ser no mínimo 0 e no máximo 999!")
